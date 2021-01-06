1ecfc5ae116cb8a2df3913daae4b242431f2bd18
mehdibourahla <mehdibrhl4@gmail.com>
2021-01-06 15:50:26 +0100

    - Set up the UI with Materialize. - Implement Redux. - Get and Add logs.

---
+++ src/actions/logActions.js
@@ -0,0 +1,53 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+
+// GET LOGS FROM SERVER
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+// ADD NEW LOG
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log("ADDED WITH SUCCESS");
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// SET LOADING TRUE
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const AddTechModal = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const onSubmit = () => {
+    if (firstName === "" || lastName === "") {
+      M.toast({ html: "Please enter the first and last name" });
+    } else {
+      console.log(firstName, lastName);
+      // Clear Fields
+      setFirstName("");
+      setLastName("");
+    }
+  };
+
+  return (
+    <div className='modal' id='add-tech-modal'>
+      <div className='modal-content'>
+        <h4>New Technician</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='FirstName'
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+            <label htmlFor='FirstName' className='active'>
+              First Name
+            </label>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='LastName'
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+            />
+            <label htmlFor='LastName' className='active'>
+              Last Name
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className='modal-footer'>
+        <a
+          href='#!'
+          onClick={onSubmit}
+          className='modal-close waves-effect waves-green btn'
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AddTechModal;
