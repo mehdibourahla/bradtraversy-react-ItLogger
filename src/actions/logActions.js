fabf714ea883034821867fcc460c58fea7b701e0
mehdibourahla <mehdibrhl4@gmail.com>
2021-01-06 17:25:57 +0100

    - Delete & Update

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,13 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG,
+} from "./types";
 
 // GET LOGS FROM SERVER
 export const getLogs = () => async dispatch => {
@@ -32,7 +41,6 @@ export const addLog = log => async dispatch => {
       },
     });
     const data = await res.json();
-    console.log("ADDED WITH SUCCESS");
     dispatch({
       type: ADD_LOG,
       payload: data,
@@ -44,7 +52,65 @@ export const addLog = log => async dispatch => {
     });
   }
 };
+// DELETE LOG
+export const deleteLog = id => async dispatch => {
+  try {
+    setLoading();
 
+    await fetch(`/logs/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+// UPDATE LOG
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: "PUT",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+// Set current log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+// Clear current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT,
+  };
+};
 // SET LOADING TRUE
 export const setLoading = () => {
   return {
